778e4e5dd5de32b663df8856a96fd1930cd36774
Tom Yos <yosshii0033@gmail.com>
2019-11-17 18:34:03 +0900

    parser3

--- parse.go
+++ parse.go
@@ -1,16 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"encoding/json"
+	"fmt"
+	"os"
 
-    "./parser"
+	"./parser"
 )
 
 func main() {
-    arg := os.Args[1]
-    scanner := new(parser.Scanner)
-    scanner.Init(arg)
-    ast := parser.Parse(scanner)
-    fmt.Printf("%#v", ast)
+	arg := os.Args[1]
+	scanner := new(parser.Scanner)
+	scanner.Init(arg)
+	ast := parser.Parse(scanner)
+
+	resultMap := parser.Eval(ast)
+	jsonObj := toJSON(resultMap)
+	result, _ := json.Marshal(jsonObj)
+	fmt.Print(string(result))
+}
+
+func toJSON(originalMap map[rune]bool) map[string]bool {
+	m := map[string]bool{}
+	for k, v := range originalMap {
+		m[string(k)] = v
+	}
+	return m
 }
---
+++ parser/evaluator.go
@@ -0,0 +1,77 @@
+package parser
+
+import "container/ring"
+
+var r = ring.New(len(weekElements))
+var weekElements = [7]rune{'月', '火', '水', '木', '金', '土', '日'}
+
+func init() {
+
+	for i := 0; i < len(weekElements); i++ {
+		r.Value = weekElements[i]
+
+		r = r.Next()
+	}
+}
+
+func Eval(e Expression) map[rune]bool {
+	var weekExists = map[rune]bool{
+		'月': false,
+		'火': false,
+		'水': false,
+		'木': false,
+		'金': false,
+		'土': false,
+		'日': false,
+	}
+	return calc(e, weekExists)
+}
+
+func calc(e Expression, weekExists map[rune]bool) map[rune]bool {
+	switch e.(type) {
+	case BinOpExpr:
+		switch e.(BinOpExpr).operator {
+		case '~':
+			left := e.(BinOpExpr).left.(WeekExpr).literal
+			right := e.(BinOpExpr).right.(WeekExpr).literal
+			weekExists = createWeekRange(left, right, weekExists)
+		case '･':
+			weekExists = calc(e.(BinOpExpr).left, weekExists)
+			weekExists = calc(e.(BinOpExpr).right, weekExists)
+		}
+	case WeekExpr:
+		weekExists[e.(WeekExpr).literal] = true
+	}
+	return weekExists
+}
+
+func Error(e string) {
+	panic(e)
+}
+
+func createWeekRange(first rune, last rune, weekExists map[rune]bool) map[rune]bool {
+	i := 0
+	for {
+		if i == len(weekElements) {
+			panic("not exists")
+		}
+		if r.Value.(rune) == first {
+			break
+		}
+		i++
+		r = r.Next()
+	}
+	j := 0
+	for {
+		if j == len(weekElements) {
+			panic("not exists")
+		}
+		weekExists[r.Value.(rune)] = true
+		if r.Value.(rune) == last {
+			break
+		}
+		j++
+		r = r.Next()
+	}
+	return weekExists
+}
